ecd3259fb709ccf435e97e4f51329d063e116bf9
Raiden7 <pascu1987@hotmail.com>
2017-07-06 19:35:47 +0200

    Prueba inicial

---
+++ Ejercicio04-David/Ejercicio04-David.ino
@@ -0,0 +1,34 @@
+#include <Servo.h>
+//Mover el servo entre las posiciones de 10º a 170º en función del valor leído
+//en el potenciómetro.
+
+//Puerto del servo
+#define PIN_SERVO 9
+//Puerto del potenciometro
+#define PIN_POT A0
+int estadoInicial=0;
+int estadoActual=0;
+Servo miservo;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Por favor usuario mueve el potenciometro");
+  miservo.attach(PIN_SERVO);
+  estadoInicial = analogRead(PIN_POT);
+  estadoActual = analogRead(PIN_POT);
+}
+
+void loop() {
+  //leo y guardo el valor registrado por el pin del potenciometro
+  if (estadoInicial != estadoActual) {
+    int val = analogRead(PIN_POT);
+    //Guardo los valores (valor recogiso por el potenciometro, valor min, valor max, 10 grados, 170 grados)
+    val = map(val, 0, 1023, 10, 170);
+    //mueve el servo
+    miservo.write(val);
+    Serial.println(val);
+    delay(15);
+    estadoInicial = estadoActual;
+  }
+  estadoActual = analogRead(PIN_POT);
+}
